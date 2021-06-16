0aeeb60bd7d1282c6f6c7d32fb946923af2f0df8
Elisabeth <elisabeth.fagnan@gmail.com>
2021-06-16 15:27:58 -0400

    Correction du path de BD dans la tracklist

--- src/tracklist.ts
+++ src/tracklist.ts
@@ -7,84 +7,84 @@
             tracknumber: 1,
             trackname: 'LES GRADINS (XII)',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/1-LES_GRADINS(XII)',
-            bdPath: '../assets/LES GRADINS_BD_WEB/1-LES_GRADINS(XII)'
+            bdPath: '../assets/LES GRADINS_BD_WEB/1-LES_GRADINS(XII)/1-LES_GRADINS(XII)_'
         },
 
         {
             tracknumber: 2,
             trackname: 'REPEAT',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/2-REPEAT',
-            bdPath: '../assets/LES GRADINS_BD_WEB/2-REPEAT'
+            bdPath: '../assets/LES GRADINS_BD_WEB/2-REPEAT/2-REPEAT_'
         },
 
         {
             tracknumber: 3,
             trackname: 'MOIMAN',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/3-MOIMANT',
-            bdPath: '../assets/LES GRADINS_BD_WEB/3-MOIMAN'
+            bdPath: '../assets/LES GRADINS_BD_WEB/3-MOIMAN/3-MOIMAN_'
         },
 
         {
             tracknumber: 4,
             trackname: 'SUNSHINE (III)',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/4-SUNSHINE(III)',
-            bdPath: '../assets/LES GRADINS_BD_WEB/4-SUNSHINE(III)'
+            bdPath: '../assets/LES GRADINS_BD_WEB/4-SUNSHINE(III)/4-SUNSHINE(III)_'
         },
 
         {
             tracknumber: 5,
             trackname: 'CARRÉ SAINT-LOUIS',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/5-CARRÉ_SAINT-LOUIS',
-            bdPath: '../assets/LES GRADINS_BD_WEB/5-CARRÉ_SAINT-LOUIS'
+            bdPath: '../assets/LES GRADINS_BD_WEB/5-CARRÉ_SAINT-LOUIS/5-CARRÉ_SAINT-LOUIS_'
         },
 
         {
             tracknumber: 6,
             trackname: 'VRAI',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/6-VRAI',
-            bdPath: '../assets/LES GRADINS_BD_WEB/6-VRAI'
+            bdPath: '../assets/LES GRADINS_BD_WEB/6-VRAI/6-VRAI_'
         },
 
         {
             tracknumber: 7,
             trackname: 'YO KODAK! (VI)',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/7-YO KODAK!(VI)',
-            bdPath: '../assets/LES GRADINS_BD_WEB/27-YO KODAK!(VI)'
+            bdPath: '../assets/LES GRADINS_BD_WEB/7-YO KODAK!(VI)/7-YO KODAK!(VI)_'
         },
 
         {
             tracknumber: 8,
             trackname: 'IMMORTEL',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/8-IMMORTEL',
-            bdPath: '../assets/LES GRADINS_BD_WEB/8-IMMORTEL'
+            bdPath: '../assets/LES GRADINS_BD_WEB/8-IMMORTEL/8-IMMORTEL_'
         },
 
         {
             tracknumber: 9,
             trackname: 'L\'ÉQUIPE',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/9-L\'ÉQUIPE',
-            bdPath: '../assets/LES GRADINS_BD_WEB/9-L\'ÉQUIPE'
+            bdPath: '../assets/LES GRADINS_BD_WEB/9-L\'ÉQUIPE/9-L\'ÉQUIPE_'
         },
 
         {
             tracknumber: 10,
             trackname: 'CARNAVAL (IX)',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/10-CARNAVAL(IX)',
-            bdPath: '../assets/LES GRADINS_BD_WEB/10-CARNAVAL(IX)'
+            bdPath: '../assets/LES GRADINS_BD_WEB/10-CARNAVAL(IX)/10-CARNAVAL(IX)_'
         },
 
         {
             tracknumber: 11,
             trackname: 'DANS LE VENT',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/11-DANS_LE_VENT',
-            bdPath: '../assets/LES GRADINS_BD_WEB/11-DANS_LE_VENT'
+            bdPath: '../assets/LES GRADINS_BD_WEB/11-DANS_LE_VENT/11-DANS_LE_VENT_'
         },
 
         {
             tracknumber: 12,
             trackname: 'OLYMPE',
             photosPath: '../assets/LES GRADINS_PHOTO_WEB/12-OLYMPE',
-            bdPath: '../assets/LES GRADINS_BD_WEB/12-OLYMPE'
+            bdPath: '../assets/LES GRADINS_BD_WEB/12-OLYMPE/12-OLYMPE_'
         }
 
      ];
