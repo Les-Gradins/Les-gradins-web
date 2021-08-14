990effe9dfee6bc17188968c6d5ed2832656f148
Mathieu Bélanger <mbelanger.poly@gmail.com>
2021-08-14 18:12:49 -0400

    Add animations and tentative global music player (test)

---
+++ src/app/app.animations.ts
@@ -0,0 +1,11 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+
+
+export const fadeAnimation = trigger('fadeAnimation', [
+  transition(':enter', [
+    style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]
+  ),
+  transition(':leave',
+    [style({ opacity: 1 }), animate('300ms', style({ opacity: 0 }))]
+  )
+]);
