7a938490e1058e26896d43b684649759e1d2ea3b
Elisabeth Fagnan <elisabeth.fagnan@gmail.com>
2021-08-17 19:59:35 -0400

    Remove les controls

--- src/app/pages/audio-player/audio-player.component.ts
+++ src/app/pages/audio-player/audio-player.component.ts
@@ -35,6 +35,19 @@ export class AudioPlayerComponent implements OnInit {
   ];
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.init();
+    }, 100);
+  }
+
+  init():void {
+    const controls = document.getElementsByClassName('controls');
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < controls.length; i++) {
+      const e = controls[i] as HTMLElement;
+      e.remove();
+
+    }
   }
 
 }
