edea474cd7bc4ddb952115c15f4a7aa3f102408f
Elisabeth Fagnan <elisabeth.fagnan@gmail.com>
2021-09-16 00:45:14 -0400

    Ajout photos compressées

--- src/saisons.ts
+++ src/saisons.ts
@@ -5,7 +5,7 @@
         {
             saisonIndex: 1,
             nomSaison: 'ÉTÉ',
-            photosPath: '/assets/LES_GRADINS_PHOTOS_WEB/ETE/ETE',
+            photosPath: '/assets/LES_GRADINS_PHOTOS_WEB_compressed/prise1000/ETE',
             tracks: ['REPEAT', 'MOIMAN', 'SUNSHINE (III)'],
             casenumber: 26,
             rotationDeg: 371.5
@@ -14,7 +14,7 @@
         {
             saisonIndex: 2,
             nomSaison: 'AUTOMNE',
-            photosPath: '/assets/LES_GRADINS_PHOTOS_WEB/AUTOMNE/AUTOMNE',
+            photosPath: '/assets/LES_GRADINS_PHOTOS_WEB_compressed/prise1000/AUTOMNE',
             tracks: ['CARRÉ SAINT-LOUIS', 'VRAI', 'YO KODAK! (VI)'],
             casenumber: 23,
             rotationDeg: 99.2
@@ -23,7 +23,7 @@
         {
             saisonIndex: 3,
             nomSaison: 'HIVER',
-            photosPath: '/assets/LES_GRADINS_PHOTOS_WEB/HIVER/HIVER',
+            photosPath: '/assets/LES_GRADINS_PHOTOS_WEB_compressed/prise1000/HIVER/HIVER',
             tracks: ['IMMORTEL', 'L\'ÉQUIPE', 'CARNAVAL (IX)'],
             casenumber: 25,
             rotationDeg: 189
@@ -32,7 +32,7 @@
         {
             saisonIndex: 4,
             nomSaison: 'PRINTEMPS',
-            photosPath: '/assets/LES_GRADINS_PHOTOS_WEB/PRINTEMPS/PRINTEMPS',
+            photosPath: '/assets/LES_GRADINS_PHOTOS_WEB_compressed/prise1000/PRINTEMPS',
             tracks: ['DANS LE VENT', 'OLYMPE', 'LES GRADINS (XII)'],
             casenumber: 19,
             rotationDeg: 281.5
